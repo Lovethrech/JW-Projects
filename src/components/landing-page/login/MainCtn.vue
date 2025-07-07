b96343f8c609f67c1e5274dd05861aec987242fd
Lovethrech <rachealdolapo45@gmail.com>
2025-07-07 23:32:38 +0100

    Working on the login page...

--- src/components/landing-page/login/MainCtn.vue
+++ src/components/landing-page/login/MainCtn.vue
@@ -1,11 +1,13 @@
 <script setup>
 import WelcomeTab from '../WelcomeTab.vue';
+import MobileWelcomeIconCtn from "@/components/landing-page/MobileWelcomeIconCtn.vue";
 </script>
 
 <template>
     <div class="main-ctn">
         <div class="main-ctn-mini-ctn">
             <WelcomeTab></WelcomeTab>
+            <MobileWelcomeIconCtn></MobileWelcomeIconCtn>
         </div>
     </div>
 </template>
