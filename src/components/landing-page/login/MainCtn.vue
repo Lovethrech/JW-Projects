154c7e1564b89c916951874d521737869c0ba4e5
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 00:44:08 +0100

    Working on the login page...

--- src/components/landing-page/login/MainCtn.vue
+++ src/components/landing-page/login/MainCtn.vue
@@ -1,13 +1,12 @@
 <script setup>
-import WelcomeTab from '../WelcomeTabContentCtn.vue';
-import MobileWelcomeIconCtn from "@/components/landing-page/MobileWelcomeIconCtn.vue";
+import WelcomeTabContentCtn from '../WelcomeTabContentCtn.vue';
+
 </script>
 
 <template>
     <div class="main-ctn">
         <div class="main-ctn-mini-ctn">
-            <WelcomeTab></WelcomeTab>
-            <MobileWelcomeIconCtn class="mobile-welcome-icon-ctn"></MobileWelcomeIconCtn>
+            <WelcomeTabContentCtn></WelcomeTabContentCtn>
         </div>
     </div>
 </template>
@@ -16,18 +15,5 @@ import MobileWelcomeIconCtn from "@/components/landing-page/MobileWelcomeIconCtn
 .main-ctn-mini-ctn{
     width:100%;
     height:100%;
-}
-.main-ctn-mini-ctn{
-    display:flex;
-    justify-content: space-between;
-    flex-direction: column;
-}
-.mobile-welcome-icon-ctn{
-    display:none;
-}
-@media screen and (max-width:890px){
-    .mobile-welcome-icon-ctn{
-        display: flex;
-    }
 }
 </style>
