91fd4ffa386ae1eab5659f3fbea743d29004595c
Lovethrech <rachealdolapo45@gmail.com>
2025-07-07 22:16:09 +0100

    Starting with the login page...

--- src/components/landing-page/WelcomeTab.vue
+++ src/components/landing-page/WelcomeTab.vue
@@ -1,3 +1,8 @@
+<script setup>
+import WelcomeTabImgCtn from './WelcomeTabImgCtn.vue';
+import WelcomeTabTextCtn from './WelcomeTabTextCtn.vue';
+</script>
+
 <template>
     
 </template>
--- src/components/landing-page/login/MainCtn.vue
+++ src/components/landing-page/login/MainCtn.vue
@@ -1,9 +1,11 @@
 <script setup>
 import WelcomeTab from '../WelcomeTab.vue';
-import WelcomeTabImgCtn from '../WelcomeTabImgCtn.vue';
-import WelcomeTabTextCtn from '../WelcomeTabTextCtn.vue';
 </script>
 
 <template>
-    
+    <div class="main-ctn">
+        <div class="main-ctn-mini-ctn">
+            <WelcomeTab></WelcomeTab>
+        </div>
+    </div>
 </template>
