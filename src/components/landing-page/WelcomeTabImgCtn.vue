74bc7c62df5e653c2e8fa20a556badb6adb5d55f
Lovethrech <rachealdolapo45@gmail.com>
2025-07-07 22:54:54 +0100

    Working on the login page...

--- src/components/landing-page/WelcomeTabImgCtn.vue
+++ src/components/landing-page/WelcomeTabImgCtn.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="welcome-tab-img-ctn">
-        <img src="" alt="">
+        <img src="/public/cuate.svg" alt="">
     </div>
 </template>
+
+<style scoped>
+.welcome-tab-img-ctn{
+    padding:50px 0;
+}
+</style>
