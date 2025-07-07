eda2c7385b85219ba07009938c6a717fa99aec38
Lovethrech <rachealdolapo45@gmail.com>
2025-07-07 22:14:52 +0100

    Starting with the login page...

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,10 @@
 <script setup>
-
+import MainCtn from '@/components/landing-page/login/MainCtn.vue';
+import MainCtn from '@/components/landing-page/login/MainCtn.vue';
 </script>
 
 <template>
   <main>
+    <MainCtn></MainCtn>
   </main>
 </template>
