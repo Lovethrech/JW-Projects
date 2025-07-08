18b249fc3256a24bb1dde535086e7a009ebd682b
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 22:45:40 +0100

    starting with the user home view components...

--- src/components/landing-page/signUp/MainCtn.vue
+++ src/components/landing-page/signUp/MainCtn.vue
@@ -4,8 +4,8 @@ import ContentCtn from './ContentCtn.vue';
 </script>
 
 <template>
-    <div class="main-ctn">
-        <div class="main-ctn-mini-ctn">
+    <div class="auth-main-ctn">
+        <div class="auth-main-ctn-mini-ctn">
             <WelcomeTabMainCtn></WelcomeTabMainCtn>
             <ContentCtn></ContentCtn>
         </div>
--- src/components/user/sideNav/MainCtn.vue
+++ src/components/user/sideNav/MainCtn.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <div class="main-ctn">
+        <div class="main-ctn-mini-ctn">
+            <section class="top-section">
+                <header>
+
+                </header>
+                <section class="nav-contents">
+
+                </section>
+            </section>
+            
+        </div>
+    </div>
 </template>
