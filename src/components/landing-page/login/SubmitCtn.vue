36430e822c9d243811988f7089c3536e7c4c8c15
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 16:25:56 +0100

    Working on the login page view and components...

--- src/components/landing-page/login/SubmitCtn.vue
+++ src/components/landing-page/login/SubmitCtn.vue
@@ -2,6 +2,7 @@
 import {ref, defineProps} from "vue";
 
 const {name, nameFor, nameType, nameLink}=defineProps(['name', 'nameFor', 'nameType', 'nameLink']);
+console.log(nameLink)
 </script>
 
 <template>
@@ -18,8 +19,16 @@ const {name, nameFor, nameType, nameLink}=defineProps(['name', 'nameFor', 'nameT
 .submit-ctn{
     width:100%;
     background-color: #7158FF;
+    display:flex;
+    justify-content: center;
+    padding:10px;
+    border-radius:10px;
 }
 .submit-ctn-link-button{
     background-color: transparent;
+    color:#ffffff;
+    font-size:20px;
+    border:0;
+    text-transform: capitalize;
 }
 </style>
