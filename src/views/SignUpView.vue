22d0ff179bd4fa88171bb22e9d3dd0e374c66e73
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 16:38:03 +0100

    Working on the signUp page view and components...

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,3 +1,17 @@
+<script setup>
+import MainCtn from '@/components/landing-page/signUp/MainCtn.vue';
+</script>
+
 <template>
-    
+    <main>
+        <MainCtn></MainCtn>
+    </main> 
 </template>
+
+<style scoped>
+@media screen and (max-width:890px){
+    main{
+        overflow: hidden;
+    }
+}
+</style>
