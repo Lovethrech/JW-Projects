207d0ce183e1b87ae682f119663f6ddba92653ae
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 15:16:40 +0100

    Working on the login page view and components...

--- src/components/landing-page/login/InputCtn.vue
+++ src/components/landing-page/login/InputCtn.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="input-ctn">
-        <label for="email" class="name"></label>
+        <label for="email" class="name">
+            Email
+        </label>
+        <input 
+            v-model="vModel"
+            :id="nameId"
+            :type="{nameType}"
+            @click="iClick"
+            @focusout="displayError"
+            class="auth-input-page-input-value"
+        />
     </div>
 </template>
