69f44f896cc40a2325e2b4869bbecb849986fdfc
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 01:22:10 +0100

    Working on the login page...

--- src/components/landing-page/login/ContentCtn.vue
+++ src/components/landing-page/login/ContentCtn.vue
@@ -1,7 +1,12 @@
+<script setup>
+import ContentDescCtn from './ContentDescCtn.vue';
+import InputCtn from './InputCtn.vue';
+</script>
+
 <template>
     <div class="content-ctn">
         <div class="content-ctn-mini-ctn">
-            
+            <ContentDescCtn></ContentDescCtn>
         </div>
     </div>
 </template>
--- src/components/landing-page/login/MainCtn.vue
+++ src/components/landing-page/login/MainCtn.vue
@@ -1,11 +1,13 @@
 <script setup>
 import WelcomeTabMainCtn from '@/components/landing-page/WelcomeTabMainCtn.vue';
+import ContentCtn from './ContentCtn.vue';
 </script>
 
 <template>
     <div class="main-ctn">
         <div class="main-ctn-mini-ctn">
             <WelcomeTabMainCtn></WelcomeTabMainCtn>
+            <ContentCtn></ContentCtn>
         </div>
     </div>
 </template>
