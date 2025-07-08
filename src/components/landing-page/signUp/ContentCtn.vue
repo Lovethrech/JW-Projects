6ca8931f8607a634b33462401e6c56ce13056314
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 17:00:38 +0100

    Working on the signUp page view and components...

--- src/components/landing-page/signUp/ContentCtn.vue
+++ src/components/landing-page/signUp/ContentCtn.vue
@@ -11,26 +11,80 @@ import SubmitCtn from './SubmitCtn.vue';
             <ContentDescCtn></ContentDescCtn>
             <form class="form">
                 <InputCtn
-                    :name="authPageContents[0].contents[0].name"
-                    :nameFor="authPageContents[0].contents[0].nameFor"
-                    :nameType="authPageContents[0].contents[0].nameType"
+                    :name="authPageContents[1].contents[0].name"
+                    :nameFor="authPageContents[1].contents[0].nameFor"
+                    :nameType="authPageContents[1].contents[0].nameType"
                 >
                 </InputCtn>
-                <br/>
                 <br/>
                 <InputCtn
-                    :name="authPageContents[0].contents[1].name"
-                    :nameFor="authPageContents[0].contents[1].nameFor"
-                    :nameType="authPageContents[0].contents[1].nameType"
+                    :name="authPageContents[1].contents[1].name"
+                    :nameFor="authPageContents[1].contents[1].nameFor"
+                    :nameType="authPageContents[1].contents[1].nameType"
                 >
                 </InputCtn>
                 <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[2].name"
+                    :nameFor="authPageContents[1].contents[2].nameFor"
+                    :nameType="authPageContents[1].contents[2].nameType"
+                >
+                </InputCtn>
+                <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[3].name"
+                    :nameFor="authPageContents[1].contents[3].nameFor"
+                    :nameType="authPageContents[1].contents[3].nameType"
+                >
+                </InputCtn>
+                <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[4].name"
+                    :nameFor="authPageContents[1].contents[4].nameFor"
+                    :nameType="authPageContents[1].contents[4].nameType"
+                >
+                </InputCtn>
+                <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[5].name"
+                    :nameFor="authPageContents[1].contents[5].nameFor"
+                    :nameType="authPageContents[1].contents[5].nameType"
+                >
+                </InputCtn>
+                <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[6].name"
+                    :nameFor="authPageContents[1].contents[6].nameFor"
+                    :nameType="authPageContents[1].contents[6].nameType"
+                >
+                </InputCtn>
+                <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[7].name"
+                    :nameFor="authPageContents[1].contents[7].nameFor"
+                    :nameType="authPageContents[1].contents[7].nameType"
+                >
+                </InputCtn>
+                <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[8].name"
+                    :nameFor="authPageContents[1].contents[8].nameFor"
+                    :nameType="authPageContents[1].contents[8].nameType"
+                >
+                </InputCtn>
+                <br/>
+                <InputCtn
+                    :name="authPageContents[1].contents[9].name"
+                    :nameFor="authPageContents[1].contents[9].nameFor"
+                    :nameType="authPageContents[1].contents[9].nameType"
+                >
+                </InputCtn>
                 <br/>
                 <SubmitCtn
-                    :name="authPageContents[0].contents[2].name"
-                    :nameFor="authPageContents[0].contents[2].nameFor"
-                    :nameType="authPageContents[0].contents[2].nameType"
-                    :nameLink="authPageContents[0].contents[2].nameLink"
+                    :name="authPageContents[1].contents[10].name"
+                    :nameFor="authPageContents[1].contents[10].nameFor"
+                    :nameType="authPageContents[1].contents[10].nameType"
+                    :nameLink="authPageContents[1].contents[10].nameLink"
                 >
                 </SubmitCtn>
             </form>
