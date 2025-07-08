31a803c9ff9375d8408938ee3765b2eba4cb2b55
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 16:39:37 +0100

    Working on the signUp page view and components...

--- src/components/landing-page/signUp/MainCtn.vue
+++ src/components/landing-page/signUp/MainCtn.vue
@@ -1,3 +1,35 @@
+<script setup>
+
+</script>
+
 <template>
     
 </template>
+
+<!-- <script setup>
+import WelcomeTabMainCtn from '@/components/landing-page/WelcomeTabMainCtn.vue';
+import ContentCtn from './ContentCtn.vue';
+</script>
+
+<template>
+    <div class="main-ctn">
+        <div class="main-ctn-mini-ctn">
+            <WelcomeTabMainCtn></WelcomeTabMainCtn>
+            <ContentCtn></ContentCtn>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.main-ctn-mini-ctn{
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction: row;
+}
+@media screen and (max-width:890px){
+    .main-ctn-mini-ctn{
+        width:200vw;
+    }
+}
+</style> -->
