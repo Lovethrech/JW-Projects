5f04aa10f91a8f77be94ad4b3da323e917fab2ad
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 22:51:01 +0100

    working on the user home view components...

--- src/components/user/sideNav/MainCtn.vue
+++ src/components/user/sideNav/MainCtn.vue
@@ -9,7 +9,16 @@
 
                 </section>
             </section>
-            
+
         </div>
     </div>
 </template>
+
+<style scoped>
+.main-ctn{
+    position:fixed;
+    background-color: #d9d9d9 ;
+    height:100vh;
+    width: 15vw;
+}
+</style>
--- src/views/UserHomeView.vue
+++ src/views/UserHomeView.vue
@@ -1,5 +1,11 @@
+<script setup>
+import MainCtn from '@/components/user/sideNav/MainCtn.vue';
+</script>
+
 <template>
     <div class="user-home-view">
-        <!-- #DEAE0F -->
+        <main>
+            <MainCtn></MainCtn>
+        </main>
     </div>
 </template>
