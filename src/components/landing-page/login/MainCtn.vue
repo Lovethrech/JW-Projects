b1ff54a9fc65352408d55f1f25c67955f5a08ac7
Lovethrech <rachealdolapo45@gmail.com>
2025-07-08 01:11:28 +0100

    Working on the login page...

--- src/components/landing-page/login/MainCtn.vue
+++ src/components/landing-page/login/MainCtn.vue
@@ -10,23 +10,9 @@ import WelcomeTabMainCtn from '@/components/landing-page/WelcomeTabMainCtn.vue';
     </div>
 </template>
 
-
-<!-- <script setup>
-import WelcomeTabMainCtn from '@/components/landing-page/WelcomeTabMainCtn.vue';
-
-</script>
-
-<template>
-    <div class="main-ctn">
-        <div class="main-ctn-mini-ctn">
-            <WelcomeTabMainCtn></WelcomeTabMainCtn>
-        </div>
-    </div>
-</template>
-
 <style scoped>
 .main-ctn-mini-ctn{
     width:100%;
     height:100%;
-} -->
-<!-- </style> -->
+}
+</style>
