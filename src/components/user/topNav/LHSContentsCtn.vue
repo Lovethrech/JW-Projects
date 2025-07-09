6e79d6a71b1e1a636209c8fff2b3ed2f26bdcf1e
Lovethrech <rachealdolapo45@gmail.com>
2025-07-09 12:25:56 +0100

    working on the user home view components...

--- src/components/user/topNav/LHSContentsCtn.vue
+++ src/components/user/topNav/LHSContentsCtn.vue
@@ -4,9 +4,28 @@
             <div class="product-overview-icon">
                 <img src="@/components/Icons/overview-icon.svg" alt=""/>
             </div>
-            <p class="title">
+            <p class="lhs-content-ctn-main-ctn-title">
                 Product Overview
             </p>
         </div>
     </div>
 </template>
+
+<style scoped>
+.lhs-content-ctn-main-ctn{
+    margin: 10px 0;
+}
+.lhs-content-ctn-mini-ctn{
+    display:flex;
+    flex-direction: row;
+    gap:10px;
+}
+.product-overview-icon{
+    background-color: #d9d9d9;
+    padding:10px;
+    border-radius: 50%;;
+}
+.lhs-content-ctn-main-ctn-title{
+    margin: auto 0;
+}
+</style>
--- src/components/user/topNav/RHSContentsCtn.vue
+++ src/components/user/topNav/RHSContentsCtn.vue
@@ -1,3 +1,17 @@
 <template>
-    
+    <div class="rhs-content-ctn-main-ctn">
+        <div class="rhs-content-ctn-mini-ctn">
+            <div class="rhs-content-ctn-main-ctn-notification-ctn">
+                <p class="rhs-content-ctn-main-ctn-notification-ctn-title">
+                    Notification
+                </p>
+                <div class="rhs-content-ctn-main-ctn-notification-ctn-icon-ctn">
+                    <img src="@/components/Icons/notification-icon.svg" alt=""/>
+                </div>
+            </div>
+            <div class="rhs-content-ctn-main-ctn-user-img-ctn">
+                <img src="" alt=""/>
+            </div>
+        </div>
+    </div>
 </template>
