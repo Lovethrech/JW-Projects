81b79cca8b58731d6475ce55a55aec0f2cfebfa9
Lovethrech <rachealdolapo45@gmail.com>
2025-07-09 14:19:45 +0100

    working on the user home view components...

--- src/components/user/mainContent/MainCatalogListNav.vue
+++ src/components/user/mainContent/MainCatalogListNav.vue
@@ -1,7 +1,13 @@
+<script setup>
+import {defineProps} from "vue";
+
+const {name}=defineProps(['name']);
+</script>
+
 <template>
     <div class="main-catalog-list-nav-main-ctn">
         <p class="main-catalog-list-nav-main-ctn-name">
-            Product name
+            {{ name }}
         </p>
     </div>
 </template>
--- src/components/user/mainContent/MainCatalogListNavCtn.vue
+++ src/components/user/mainContent/MainCatalogListNavCtn.vue
@@ -1,11 +1,15 @@
 <script setup>
+import userMainCatalogNavLists from "@/data/user-main-catalog-nav-lists.json";
 import MainCatalogListNav from './MainCatalogListNav.vue';
 </script>
 
 <template>
     <div class="main-catalog-list-nav-ctn-main-ctn">
         <div class="main-catalog-list-nav-ctn-mini-ctn">
-            <MainCatalogListNav></MainCatalogListNav>
+            <MainCatalogListNav
+                
+            >
+            </MainCatalogListNav>
         </div>
     </div>
 </template>
