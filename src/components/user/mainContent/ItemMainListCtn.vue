c65caf1f2e6ef6d4f5d42ff03ea32e2d4e9bd3fc
Lovethrech <rachealdolapo45@gmail.com>
2025-07-09 13:15:52 +0100

    working on the user home view components...

--- src/components/user/mainContent/ItemMainListCtn.vue
+++ src/components/user/mainContent/ItemMainListCtn.vue
@@ -1,3 +1,7 @@
 <template>
-    
+    <div class="user-main-content-item-main-list-ctn-main-ctn">
+        <div class="user-main-content-item-main-list-ctn-mini-ctn">
+            
+        </div>
+    </div>
 </template>
