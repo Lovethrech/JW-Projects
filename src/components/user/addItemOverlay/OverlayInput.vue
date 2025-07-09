e50ba78111f73cbb3925ecde9faf67d66a53b5c8
Lovethrech <rachealdolapo45@gmail.com>
2025-07-09 16:19:08 +0100

    working on the user home view components...

--- src/components/user/addItemOverlay/OverlayInput.vue
+++ src/components/user/addItemOverlay/OverlayInput.vue
@@ -1,7 +1,21 @@
 <template>
     <div class="user-add-item-overlay-input-main-ctn">
         <div class="user-add-item-overlay-input-mini-ctn">
+            <label 
+                for="nameFor" 
+                class="user-add-item-overlay-input-mini-ctn-input-ctn-name"
+            >
             
+            </label>
+            <input 
+                id="nameFor"
+                type="nameType"
+                class="user-add-item-overlay-input-mini-ctn-input"
+            />
         </div>
     </div>
 </template>
+
+<style scoped>
+
+</style>
