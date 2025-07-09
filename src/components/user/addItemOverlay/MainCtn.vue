d8639524e914dcf3ccabaebe4a0a863772eb6f85
Lovethrech <rachealdolapo45@gmail.com>
2025-07-09 15:39:16 +0100

    working on the user home view components...

---
+++ src/components/user/addItemOverlay/MainCtn.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="user-add-item-overlay-main-ctn-main-ctn">
+        <div class="user-add-item-overlay-main-ctn-mini-ctn">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.user-add-item-overlay-main-ctn-main-ctn{
+    background-color: chocolate;
+    width:90vw;
+    height:90vh
+}
+.user-add-item-overlay-main-ctn-mini-ctn{
+
+}
+</style>
