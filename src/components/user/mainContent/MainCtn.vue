1f7b74d2e02c94955d23f91863ab310e6fe36d41
Lovethrech <rachealdolapo45@gmail.com>
2025-07-09 13:14:12 +0100

    working on the user home view components...

--- src/components/user/mainContent/MainCtn.vue
+++ src/components/user/mainContent/MainCtn.vue
@@ -1,7 +1,13 @@
+<script setup>
+import ItemMainListCtn from './ItemMainListCtn.vue';
+import AddItemTab from './AddItemTab.vue';
+</script>
+
 <template>
     <div class="user-main-content-main-ctn-main-ctn">
         <div class="user-main-content-main-ctn-mini-ctn">
-            
+            <ItemMainListCtn></ItemMainListCtn>
+            <AddItemTab></AddItemTab>
         </div>
     </div>
 </template>
