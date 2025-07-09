f208fb8885b68152329cfabc76a1d714685499c8
Lovethrech <rachealdolapo45@gmail.com>
2025-07-09 16:12:37 +0100

    working on the user home view components...

--- src/components/user/addItemOverlay/OverlayInputMainCtn.vue
+++ src/components/user/addItemOverlay/OverlayInputMainCtn.vue
@@ -1,7 +1,13 @@
+<script setup>
+import OverlayInput from './OverlayInput.vue';
+import OverlayInputDrop from './OverlayInputDrop.vue';
+</script>
+
 <template>
     <div class="user-add-item-overlay-input-main-ctn-main-ctn">
         <div class="user-add-item-overlay-input-main-ctn-mini-ctn">
-            
+            <OverlayInput></OverlayInput>
+            <OverlayInputDrop></OverlayInputDrop>
         </div>
     </div>
 </template>
