f3801c2a7d37d73f5a52d6ffe2994aaf24882ef6
Lovethrech <rachealdolapo45@gmail.com>
2025-07-10 04:59:49 +0100

    working on the user home view components...

---
+++ src/stores/overlayImageUploadToggle.js
@@ -0,0 +1,22 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+export const useWelcomeTabToggleStore = defineStore('welcomeTabToggle', () => {
+    const defaultMargin = "0";
+    const activeMargin = "-100vw";
+
+    const WelcomeTabActive = ref({ marginLeft: defaultMargin});
+    const resetMargin = () => {
+        WelcomeTabActive.value.marginRight = defaultMargin;
+    };
+
+    const shiftTab=()=>{
+        if (WelcomeTabActive.value.marginRight===defaultMargin){
+            WelcomeTabActive.value.marginRight=activeMargin;
+        }
+        else{
+            resetMargin();
+        }
+    }
+    return {shiftTab,WelcomeTabActive};
+});
