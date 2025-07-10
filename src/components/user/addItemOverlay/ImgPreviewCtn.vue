f9b6969f3b621617fe6239978d08b438e685d7ff
Lovethrech <rachealdolapo45@gmail.com>
2025-07-10 05:24:32 +0100

    working on the user home view components...

--- src/components/user/addItemOverlay/ImgPreviewCtn.vue
+++ src/components/user/addItemOverlay/ImgPreviewCtn.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import { useOverlayImageUploadToggleStore } from '@/stores/overlayImageUploadToggle';
+const {previewUrl}=useOverlayImageUploadToggleStore();
 </script>
 
 <template>
     <div class="user-add-item-overlay-img-preview-ctn-main-ctn">
-        <img src="" alt="">
+        <img :src="previewUrl" alt="">
     </div>
 </template>
