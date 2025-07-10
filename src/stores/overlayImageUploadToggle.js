ce1031893c4cb4eaa14187c0706d3d896f2a2766
Lovethrech <rachealdolapo45@gmail.com>
2025-07-10 15:36:37 +0100

    working on the user home view components...

--- src/stores/overlayImageUploadToggle.js
+++ src/stores/overlayImageUploadToggle.js
@@ -4,6 +4,12 @@ import { defineStore } from 'pinia';
 export const useOverlayImageUploadToggleStore = defineStore('overlayImageUploadToggle', () => {
     const imageUrl = ref('');
     function handleImageUpload(event) {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.style.display = 'none';
+
+
         const file = event.target.files[0]
         if (file && file.type.startsWith('image/')) {
             imageUrl.value = URL.createObjectURL(file)
