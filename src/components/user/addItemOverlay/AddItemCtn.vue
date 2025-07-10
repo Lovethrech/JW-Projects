8ff9ba83788e89a57857df3688a33f8bbba16073
Lovethrech <rachealdolapo45@gmail.com>
2025-07-10 02:51:45 +0100

    working on the user home view components...

--- src/components/user/addItemOverlay/AddItemCtn.vue
+++ src/components/user/addItemOverlay/AddItemCtn.vue
@@ -1,3 +1,9 @@
 <template>
-    
+    <div class="user-add-item-overlay-add-item-ctn-main-ctn">
+        <div class="user-add-item-overlay-add-item-ctn-mini-ctn">
+            <p class="user-add-item-overlay-add-item-ctn-main-ctn-name">
+                Add product
+            </p>
+        </div>
+    </div>
 </template>
