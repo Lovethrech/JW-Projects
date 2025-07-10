f4bf0bce7e0ea6e753c637ab3a20b768ffdd5ae0
Lovethrech <rachealdolapo45@gmail.com>
2025-07-10 02:49:14 +0100

    working on the user home view components...

--- src/components/user/addItemOverlay/ItemBtnMainCtn.vue
+++ src/components/user/addItemOverlay/ItemBtnMainCtn.vue
@@ -1,11 +1,13 @@
 <script setup>
-
+import AddItemCtn from './AddItemCtn.vue';
+import CancelOverlayCtn from './CancelOverlayCtn.vue';
 </script>
 
 <template>
     <div class="user-add-item-overlay-item-btn-main-ctn-main-ctn">
         <div class="user-add-item-overlay-item-btn-main-ctn-mini-ctn">
-
+            <AddItemCtn></AddItemCtn>
+            <CancelOverlayCtn></CancelOverlayCtn>
         </div>
     </div>
 </template>
