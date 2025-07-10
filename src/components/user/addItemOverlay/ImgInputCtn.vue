37d4ab89087235c402afb849262d2f205975a669
Lovethrech <rachealdolapo45@gmail.com>
2025-07-10 04:38:31 +0100

    working on the user home view components...

---
+++ src/components/user/addItemOverlay/ImgInputCtn.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="user-add-item-overlay-img-input-ctn-main-ctn">
+        <input type="file" accept="image/*"/>
+    </div>
+</template>
---
+++ src/components/user/addItemOverlay/ImgPreviewCtn.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="user-add-item-overlay-img-preview-ctn-main-ctn">
+        <p class="user-add-item-overlay-img-preview-ctn-main-ctn-title">
+            upload product image
+        </p>
+        <div class="user-add-item-overlay-img-preview-ctn-main-ctn-icon-ctn">
+            <i class="ri-add-circle-line"></i>
+        </div>
+        <p class="user-add-item-overlay-img-preview-ctn-main-ctn-desc">
+            Tap on field to add image
+        </p>
+    </div>
+</template>
