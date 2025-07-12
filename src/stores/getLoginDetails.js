078a1a2acf84dfc761cf2b9747707901233da399
Lovethrech <rachealdolapo45@gmail.com>
2025-07-12 19:48:09 +0100

    working on the user home view components...

--- src/stores/getLoginDetails.js
+++ src/stores/getLoginDetails.js
@@ -2,10 +2,8 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useGetLoginDetailsStore = defineStore('getLoginDetails', () => {
-    const loginValue=reactive({
-        email:"",
-        password:''
-    })
 
-    return {loginValue};
+    const email=ref('nameE')
+
+    return {email};
 });
