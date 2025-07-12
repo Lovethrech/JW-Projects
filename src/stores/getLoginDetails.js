5491b3962755da86a2b68efac17b10b46cc16e71
Lovethrech <rachealdolapo45@gmail.com>
2025-07-12 19:56:58 +0100

    working on the user home view components...

--- src/stores/getLoginDetails.js
+++ src/stores/getLoginDetails.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia';
 
 export const useGetLoginDetailsStore = defineStore('getLoginDetails', () => {
     const email=ref('nameE');
+    const password=ref('nameP')
 
     return {email};
 });
