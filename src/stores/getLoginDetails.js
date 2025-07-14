0d64141f4eee32e2a0fb6905662d75c7b31edd47
Lovethrech <rachealdolapo45@gmail.com>
2025-07-14 20:52:37 +0100

    working on the user home view components...

--- src/stores/getLoginDetails.js
+++ src/stores/getLoginDetails.js
@@ -2,8 +2,11 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useGetLoginDetailsStore = defineStore('getLoginDetails', () => {
+
+    const defaultBorder=ref('')
     const email=ref('nameE');
     const password=ref('namePassowrd');
+    const inputCtnBorderAlertStyle=ref('')
 
     const submitLoginDetails=()=>{
         if ((email.value==="")&&(password.value="")){
