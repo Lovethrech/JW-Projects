6f784c5d052252abd6fdfcff4f1903005ed267a3
Lovethrech <rachealdolapo45@gmail.com>
2025-07-14 21:17:15 +0100

    working on the user home view components...

--- src/stores/getLoginDetails.js
+++ src/stores/getLoginDetails.js
@@ -3,14 +3,14 @@ import { defineStore } from 'pinia';
 
 export const useGetLoginDetailsStore = defineStore('getLoginDetails', () => {
     const defaultBorder=ref('3px solid #ededed');
-    const activeBorder=ref('3px solid #C95F50');
+    const activeBorder=ref('1px solid #C95F50');
 
     const email=ref('');
     const password=ref('');
     const inputCtnBorderAlertStyle=defaultBorder;
 
     const submitLoginDetails=()=>{
-        if (((email.value==="")&&(password.value=""))|| ((email.value==="")||(password.value=""))){
+        if ((((email.value==="") && (email.value===null))&&((password.value==="") && (password.value===null)))|| (((email.value==="") && (email.value===null))||((password.value==="") && (password.value===null)))){
             inputCtnBorderAlertStyle.value=activeBorder;
         }
     }
